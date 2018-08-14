82317f57a2d9fafc9dc23c987f010848c5089229
Vicki Hamm <vicki.cello@gmail.com>
2018-08-14 23:36:34 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change
-    add_column :grade, :integer, :birthdate, :string
+    add_column :students, :grade, :integer, 
+    add_column :students, :birthdate, :string
   end
 end
