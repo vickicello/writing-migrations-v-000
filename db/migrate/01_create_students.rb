d0c790f602caf660534ec3bff9d19705f1a1b038
Vicki Hamm <vicki.cello@gmail.com>
2018-08-14 23:43:37 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    create_table :students do |s|
-      s.string :name
+    create_table :students do |t|
+      t.string :name
     end
   end
 
